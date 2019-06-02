3a8e2537ce5351770f070c8f3ba0892373490343
AnnabellAnastasia <oav2008@gmail.com>
2019-06-02 19:35:43 -0400

    alternate state intialization

--- src/index.js
+++ src/index.js
@@ -4,41 +4,32 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-
-
-        // this is the only time we do direct assigment to this.state!!!!!!
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState!!!
-                this.setState({ lat: position.coords.latitude });
-            },
-
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
-    };
-
+    }
+    componentDidUpdate() {
+        console.log('My component was just updated!')
+    }
 
 
     // React says that we need to define render
     render() {
-            if (this.state.errorMessage && ! this.state.lat) {
-                return <div> Error: {this.state.errorMessage}</div>;
-            }
-            if (!this.state.errorMessage && this.state.lat){
-                return <div> Latitude: {this.state.lat}</div>;
-            }
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude: {this.state.lat}</div>;
+        }
 
-            return <div> Loading!</div>;
-            }
-        };
-    
+        return <div> Loading!</div>;
+    }
+};
+
 
 
 ReactDOM.render(
