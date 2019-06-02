85956f3285cad84e75217596a3208db008f67bda
AnnabellAnastasia <oav2008@gmail.com>
2019-06-02 18:10:50 -0400

    conditional rendering

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+return <div>
+    Season Displsay
+</div>
+};
+ReactDOM.render(<App />, document.querySelector('#root')
+);
