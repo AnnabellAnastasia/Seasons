a7e466722e49293e76735750120464ddd19b8e68
AnnabellAnastasia <oav2008@gmail.com>
2019-06-02 21:05:05 -0400

    component to show an icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -7,13 +7,26 @@ const getSeason = (lat, month) => {
         return lat > 0 ? 'winter' : 'summer'
     }
 };
+
+
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+    const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+
+
     return (
         <div>
-            {text}
+            <i className={`${icon} icon`} />
+            <h1>
+                {text}
+            </h1>
+            <i className={`${icon} icon`} />
+
         </div>
     );
 };
+
+
 export default SeasonDisplay;
